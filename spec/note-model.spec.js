82357f51893efd7ad08e55a22ffb19eba3ae0ae2
Erin Stevenson <estevenson_94@live.com>
2019-02-22 21:23:12 +0000

    4. Test for note to take text upon instantiation

--- spec/note-model.spec.js
+++ spec/note-model.spec.js
@@ -3,3 +3,9 @@
   let note = new Note();
   assert.isEqual(note.__proto__.constructor.name === methodName);
 })();
+
+(function testNoteText() {
+  let text = "This is my first note";
+  let note = new Note(text);
+  assert.isEqual(note.returnNoteText() === text);
+})();
