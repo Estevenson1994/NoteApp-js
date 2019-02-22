85e2268957d4fc2ade5154ec52c3ce9af1890a11
Erin Stevenson <estevenson_94@live.com>
2019-02-22 22:06:35 +0000

    15. Note-list-view returns html snippet

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,4 +1,21 @@
 (function(exports) {
-  function NoteListView() {}
+  function NoteListView(noteList) {
+    this._noteList = noteList;
+  }
   exports.NoteListView = NoteListView;
 })(this);
+
+NoteListView.prototype.addNote = function(note) {
+  this._noteList.createNote(note);
+};
+
+NoteListView.prototype.getNoteList = function() {
+  return this._noteList.notes;
+};
+
+NoteListView.prototype.getHtml = function() {
+  let html = this.getNoteList().map(note => {
+    return "<li><div>" + note.returnNoteText() + "</div></li>";
+  });
+  return "<ul>" + html.join("") + "</ul>";
+};
