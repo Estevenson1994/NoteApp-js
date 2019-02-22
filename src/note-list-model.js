d7e627840bde0d851a2f2dd5ef6a6a6d0fa8387b
Erin Stevenson <estevenson_94@live.com>
2019-02-22 21:49:41 +0000

    11. Can add notes to list

--- src/note-list-model.js
+++ src/note-list-model.js
@@ -6,5 +6,14 @@
   NoteList.prototype.returnNoteList = function() {
     return this._notes;
   };
+
+  NoteList.prototype.createNote = function(note) {
+    this._notes.push(note);
+  };
+
+  NoteList.prototype.returnNoteText = function(index) {
+    return this._notes[index].returnNoteText();
+  };
+
   exports.NoteList = NoteList;
 })(this);
