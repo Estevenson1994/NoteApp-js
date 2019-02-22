12885e84770c5fc89c71dcacf3874ac24cadb531
Erin Stevenson <estevenson_94@live.com>
2019-02-22 21:48:21 +0000

    10. Test method to add notes to note-list-model

--- spec/note-list-model.spec.js
+++ spec/note-list-model.spec.js
@@ -8,3 +8,21 @@
   let noteList = new NoteList();
   assert.isEqual(noteList.returnNoteList().length === 0);
 })();
+
+(function testStoringNotesText() {
+  function NoteDouble(text) {
+    this.text = text;
+  }
+
+  NoteDouble.prototype = {
+    returnNoteText: function() {
+      return this.text;
+    }
+  };
+
+  let noteDouble = new NoteDouble("this is my first note");
+  let noteList = new NoteList();
+
+  noteList.createNote(noteDouble);
+  assert.isEqual(noteList.returnNoteText(0) === "this is my first note");
+})();
