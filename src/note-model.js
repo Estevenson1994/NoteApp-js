2cb6e92678f97c57b5510f4bc5383d1c4ccf2176
Erin Stevenson <estevenson_94@live.com>
2019-02-22 21:28:22 +0000

    5. Passed test for note to take text

--- src/note-model.js
+++ src/note-model.js
@@ -1,5 +1,11 @@
 (function(exports) {
-  function Note(text) {}
+  function Note(text) {
+    this._text = text;
+  }
+
+  Note.prototype.returnNoteText = function() {
+    return this._text;
+  };
 
   exports.Note = Note;
 })(this);
