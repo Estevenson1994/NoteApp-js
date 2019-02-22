021021ef046cf002da2bc63771f7f2f94ba944c3
Erin Stevenson <estevenson_94@live.com>
2019-02-22 22:04:45 +0000

    14.Test note list view can take a note list and return html snippet

--- spec/note-list-view.spec.js
+++ spec/note-list-view.spec.js
@@ -3,3 +3,34 @@
   let noteListView = new NoteListView();
   assert.isEqual(noteListView.__proto__.constructor.name === methodName);
 })();
+
+(function testSingleNoteListView() {
+  function NoteListDouble() {
+    this.notes = [];
+  }
+
+  function NoteDouble(text) {
+    this.text = text;
+  }
+
+  NoteListDouble.prototype = {
+    createNote: function(note) {
+      this.notes.push(note);
+    }
+  };
+
+  NoteDouble.prototype = {
+    returnNoteText: function() {
+      return this.text;
+    }
+  };
+
+  let noteDouble = new NoteDouble("first note");
+  let noteListDouble = new NoteListDouble();
+
+  let noteListView = new NoteListView(noteListDouble);
+  noteListView.addNote(noteDouble);
+  assert.isEqual(
+    noteListView.getHtml() === "<ul><li><div>first note</div></li></ul>"
+  );
+})();
