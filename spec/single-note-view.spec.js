57e29aa75132c58a6159847ad7643604b65ca808
Erin Stevenson <estevenson_94@live.com>
2019-02-23 07:45:35 +0000

    22. Test singleNoteView to return html note

--- spec/single-note-view.spec.js
+++ spec/single-note-view.spec.js
@@ -3,3 +3,20 @@
   let singleNoteView = new SingleNoteView();
   assert.isEqual(singleNoteView.__proto__.constructor.name === methodName);
 })();
+
+(function testSingleNoteViewTest() {
+  function NoteDouble(text) {
+    this.text = text;
+  }
+
+  NoteDouble.prototype = {
+    returnNoteText: function() {
+      return this.text;
+    }
+  };
+
+  let noteDouble = new NoteDouble("testing");
+  let singleNoteView = new SingleNoteView(noteDouble);
+
+  assert.isEqual(singleNoteView.getHtml() === "<div>testing</div>");
+})();
