e8bcf4ff1269bfed8f533de7c7d11dc43b0ac638
Erin Stevenson <estevenson_94@live.com>
2019-02-23 08:03:17 +0000

    25. Note list view only returns first 20 chars of note

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -3,19 +3,32 @@
     this._noteList = noteList;
   }
   exports.NoteListView = NoteListView;
+
+  NoteListView.prototype.addNote = function(note) {
+    this._noteList.createNote(note);
+  };
+
+  NoteListView.prototype.getNoteList = function() {
+    return this._noteList.returnNoteList();
+  };
+
+  NoteListView.prototype.getHtml = function() {
+    let html = this.getNoteList().map(note => {
+      return "<li><div>" + note.returnNoteText() + "</div></li>";
+    });
+    return "<ul>" + html.join("") + "</ul>";
+  };
+  NoteListView.prototype.get20Chars = function() {
+    let twentyChars = this.getNoteList().map(note => {
+      return note.returnNoteText().substring(0, 20);
+    });
+    return twentyChars;
+  };
+
+  NoteListView.prototype.getFirstTwentyCharsHTML = function() {
+    let html = this.get20Chars().map(note => {
+      return "<li><div>" + note + "</div></li>";
+    });
+    return "<ul>" + html.join("") + "</ul>";
+  };
 })(this);
-
-NoteListView.prototype.addNote = function(note) {
-  this._noteList.createNote(note);
-};
-
-NoteListView.prototype.getNoteList = function() {
-  return this._noteList.returnNoteList();
-};
-
-NoteListView.prototype.getHtml = function() {
-  let html = this.getNoteList().map(note => {
-    return "<li><div>" + note.returnNoteText() + "</div></li>";
-  });
-  return "<ul>" + html.join("") + "</ul>";
-};
