1a85693a4dce04888d4e9d61a2464b3aa1393bde
Erin Stevenson <estevenson_94@live.com>
2019-02-23 08:48:58 +0000

    33. Clicking on hrefs will show full note

--- src/note-controller.js
+++ src/note-controller.js
@@ -9,9 +9,36 @@
 
   NoteController.prototype.listNotes = function() {
     document.getElementById(
-      "app"
+      "view"
     ).innerHTML = this._noteListView.getFirstTwentyCharsHTML();
   };
 
+  makeUrlChangeShowNoteForCurrentPage();
+
+  function makeUrlChangeShowNoteForCurrentPage() {
+    window.addEventListener("hashchange", showNoteForCurrentPage);
+  }
+  function showNoteForCurrentPage() {
+    showNote(getNoteFromUrl(window.location));
+  }
+
+  function getNoteFromUrl(location) {
+    return location.hash.split("#")[1];
+  }
+
+  function showNote(noteId) {
+    var note = findNote(noteId);
+    var view = new SingleNoteView(note);
+    document.getElementById("app").innerHTML = view.getHtml();
+  }
+
+  function findNote(noteId) {
+    controller._noteList.returnNoteList().forEach(function(note) {
+      if (note.showId().toString() === noteId) {
+        thisNote = note;
+      }
+    });
+    return thisNote;
+  }
   exports.NoteController = NoteController;
 })(this);
